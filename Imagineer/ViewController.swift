5641e0d80be7184b56a2055a35893ec4731f3293
a.cheverda <a.cheverda@2Gis.ru>
2023-05-03 23:35:51 +0300

    Initial Commit

---
+++ Imagineer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imagineer
+//
+//  Created by arina on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
